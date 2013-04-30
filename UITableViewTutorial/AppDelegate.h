cfbf3a8686b96656c6bfb88a477b11e4c58ce5f1
Alexis Kinsella <alexis.kinsella@gmail.com>
2013-04-30 18:54:45 +0200

    Initial Commit

---
+++ UITableViewTutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITableViewTutorial
+//
+//  Created by Alexis Kinsella on 30/04/13.
+//  Copyright (c) 2013 Xebia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
